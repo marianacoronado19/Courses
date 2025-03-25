d99fffbbb72229b7d4ebf90fe0a8f00ea47b4cea
marianacoronado19 <maricoronad19@gmail.com>
2025-03-24 23:44:01 -0300

    Initial commit

---
+++ app/src/main/java/com/example/courses/data/DataSource.kt
@@ -0,0 +1,21 @@
+import com.example.courses.R
+import com.example.courses.model.Curso
+
+object DataSource {
+    val cursos = listOf(
+        Curso(R.string.architecture, 58, R.drawable.architecture),
+        Curso(R.string.crafts, 121, R.drawable.crafts),
+        Curso(R.string.business, 78, R.drawable.business),
+        Curso(R.string.culinary, 118, R.drawable.culinary),
+        Curso(R.string.design, 423, R.drawable.design),
+        Curso(R.string.fashion, 92, R.drawable.fashion),
+        Curso(R.string.film, 165, R.drawable.film),
+        Curso(R.string.gaming, 164, R.drawable.gaming),
+        Curso(R.string.drawing, 326, R.drawable.drawing),
+        Curso(R.string.lifestyle, 305, R.drawable.lifestyle),
+        Curso(R.string.music, 212, R.drawable.music),
+        Curso(R.string.painting, 172, R.drawable.painting),
+        Curso(R.string.photography, 321, R.drawable.photography),
+        Curso(R.string.tech, 118, R.drawable.tech)
+    )
+}
